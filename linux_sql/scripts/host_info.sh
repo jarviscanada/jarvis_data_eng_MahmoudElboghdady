64f2002cfdb73fec293b809f0222832f116f35d5
Mahmoud Elboghdady <mahmoud.elboghdady@jrvs.ca>
2026-04-05 22:42:24 +0000

    feat: implement host_info and host_usage monitoring agent scripts

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Collect hardware specification data and insert into psql
+# Setup and validate arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
 
-psql_host="localhost"
-psql_port="5432"
-db_name="host_agent"
-psql_user="postgres"
-psql_password="password"
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
 
-# Collect hardware info
+# Parse hardware specifications
 lscpu_out=$(lscpu)
 hostname=$(hostname -f)
 cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
@@ -19,7 +23,7 @@ l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
 total_mem=$(grep MemTotal /proc/meminfo | awk '{print $2}' | xargs)
 timestamp=$(date -u '+%Y-%m-%d %H:%M:%S')
 
-# Insert into hosts table
+# Insert hardware specs into host_info table
 insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp)
 VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp');"
 
